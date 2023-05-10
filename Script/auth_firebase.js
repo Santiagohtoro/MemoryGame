fb2db0eb945523b03dd9af59285f223f3f19f9ec
CristhianUrrea <CristhianUrrea@ibm.com>
2023-05-09 21:42:59 -0500

    Registry Changes. New Log.JS

---
+++ Script/auth_firebase.js
@@ -0,0 +1,47 @@
+
+// Import the functions you need from the SDKs you need
+
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDXSh0QIZhI8gGgejGEt2axn39m0JZiZnE",
+    authDomain: "memorygame-97e68.firebaseapp.com",
+    databaseURL: "https://memorygame-97e68-default-rtdb.firebaseio.com",
+    projectId: "memorygame-97e68",
+    storageBucket: "memorygame-97e68.appspot.com",
+    messagingSenderId: "1014281328293",
+    appId: "1:1014281328293:web:035d98b1d227dfbd2b9f4c",
+    measurementId: "G-57G1EXF8YX"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const auth = firebase.auth();
+  const database = firebase.firestore(); // Para Firestore, o firebase.database() para Realtime Database
+  
+  function registrarse() {
+      const nombre = document.querySelector("#nameInput");
+      const correo = document.getElementById('emailInput');
+      const password = document.getElementById('passwordInput');
+      
+      auth.createUserWithEmailAndPassword(correo.value, password.value)
+      .then(function () {
+          let user = auth.currentUser;
+          let database_ref =  database.collection("users").doc(user.uid);
+        
+          var user_data = {
+              email: correo.value,
+              nombre: nombre.value,
+              password: password.value,
+              last_login: Date.now()
+          };
+          alert(user_data)
+          database_ref.set(user_data);
+          window.alert("LISTOOO")
+      });
+     
+  }
--- Script/registry.js
+++ Script/registry.js
@@ -1,81 +1,56 @@
+
 window.addEventListener("load", function () {
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('registerForm').addEventListener('submit', validarFormulario);
+    document.addEventListener('DOMContentLoaded', function () {
+        document.getElementById('registerForm').addEventListener('submit', validarFormulario);
+    });
+
+    function validarFormulario(evento) {
+        evento.preventDefault();
+        var password = document.getElementById('passwordInput').value,
+            reviewPassword = document.getElementById('reviewPassword').value;
+
+        if (password.length < 8) {
+            alert("Hacer la contraseña mas larga");
+            document.registerForm.passwordInput.focus();
+            return 0;
+        }
+
+        if (password.match(/[a-z]/) && password.match(/[A-Z]/)) {
+            strength += 1;
+        } else {
+            alert("Usar minusculas y mayusculas");
+            document.registerForm.passwordInput.focus();
+            return 0;
+        }
+
+        if (password.match(/\d/)) {
+            strength += 1;
+        } else {
+            alert("Incluya un numero");
+            document.registerForm.passwordInput.focus();
+            return 0;
+        }
+
+        if (password.match(/[^a-zA-Z\d]/)) {
+            strength += 1;
+        } else {
+            alert("Incluya un caracter especial ");
+            document.registerForm.passwordInput.focus();
+            return 0;
+        }
+
+        //Campo verificar contraseñas
+        if (reviewPassword.length == 0) {
+            alert("El campo repite tu contraseña esta vacio");
+            document.registerForm.reviewPassword.focus();
+            return 0;
+        }
+
+        if (reviewPassword != password) {
+            alert("Las contraseñas no son iguales");
+        }
+    }
 });
-
-function validarFormulario(evento) {
-    evento.preventDefault();
-    var nombre = document.getElementById('nameInput').value,
-        correo = document.getElementById('emailInput').value,
-        password = document.getElementById('passwordInput').value,
-        reviewPassword = document.getElementById('reviewPassword').value;
-
-    var emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-    var strength = 0;
-
-    //Campo nombre
-    if (nombre.length == 0 || nombre.length <= 9) {
-        alert("El campo nombre esta incompleto");
-        document.registerForm.nameInput.focus();
-        return 0;
-    }
-
-    //Campo correo
-    if (emailRegex.test(correo)) {
-        console.log("Correcto");
-    } else {
-        alert("El campo de correo esta incorrecto");
-        document.registerForm.emailInput.focus();
-    }
-
-    // Campo contraseña
-    if (password.length == 0) {
-        alert("El campo contraseña esta vacio");
-        document.registerForm.passwordInput.focus();
-        return 0;
-    }
-
-    if (password.length < 8) {
-        alert("Hacer la contraseña mas larga");
-        document.registerForm.passwordInput.focus();
-        return 0;
-    }
-
-    if (password.match(/[a-z]/) && password.match(/[A-Z]/)) {
-        strength += 1;
-    } else {
-        alert("Usar minusculas y mayusculas");
-        document.registerForm.passwordInput.focus();
-        return 0;
-    }
-
-    if (password.match(/\d/)) {
-        strength += 1;
-    } else {
-        alert("Incluya un numero");
-        document.registerForm.passwordInput.focus();
-        return 0;
-    }
-
-    if (password.match(/[^a-zA-Z\d]/)) {
-        strength += 1;
-    } else {
-        alert("Incluya un caracter especial ");
-        document.registerForm.passwordInput.focus();
-        return 0;
-    }
-
-    //Campo verificar contraseñas
-    if (reviewPassword.length == 0) {
-        alert("El campo repite tu contraseña esta vacio");
-        document.registerForm.reviewPassword.focus();
-        return 0;
-    }
-
-    if (reviewPassword != password) {
-        alert("Las contraseñas no son iguales");
-    }
-}
 
 function verificarCheck() {
     //Campo terminos
@@ -92,9 +67,3 @@ function verificarCheck() {
         }
     });
 }
-
-function goLogIn() {
-    location.href = '#';
-    console.log('Enviado');
-}
-});
